a49aa2a1aca147694c4f4ffe5c3dd80f445f3dd0
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-05-28 22:56:06 -0400

    finished problem

--- serial.py
+++ serial.py
@@ -20,3 +20,15 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        self.start = self.next = start
+    
+    def generate(self):
+        self.next += 1
+        return self.next - 1
+    
+    def reset(self):
+        self.next = self.start
+        
+
+
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,31 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, path):
+        file = open(path)
+        self.words = self.make_list(file)
+        
+        print(f"{len(self.words)} words read")
+        # count = 0
+
+        # for word in file:
+        #     count += 1
+
+        # print(f"{count} words read")
+
+    def make_list(self, file):
+        self.list = []
+        for line in file:
+            self.list.append(line.strip())
+
+        return self.list
+
+    def random(self):
+        return random.choice(self.words)
+        # index = randint(0, len(self.list))
+        # return self.list[index]
+
+class SpecialWordFinder(WordFinder):
+    def make_list(self, file):
+            return[word.strip() for word in file if word.strip() and not word.startswith("#")]  
